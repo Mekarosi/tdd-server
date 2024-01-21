fbe9f7a5639b61292f80c9e72addf6bdc1f4a92b
Mekarosi <mekarosi_assa@yahoo.com>
2024-01-21 13:20:56 -0800

    Wrote code to create a wrapper for our database

---
+++ src/db.test.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import { expect } from "chai";
+import { getUserByUsername } from './db'
+
+describe('getUserByUsername', () => {
+    it('get the correct user from the database given a username', async  () => {
+       const client = await MongoClient.connect(
+        `mongodb://localhost:27017/TEST_DB`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+       )
+       const db = client.db('TEST_DB')
+
+       // tests
+       client.close()
+    })
+})
