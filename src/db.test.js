efb74fb066080c81717b5e2f8162b6e30909a9b4
Mekarosi <mekarosi_assa@yahoo.com>
2024-01-23 12:32:41 -0800

    Wrote integration for server

--- src/db.test.js
+++ src/db.test.js
@@ -3,6 +3,10 @@ import { getUserByUsername } from './db.js';
 import { getDatabaseData, setDatabaseData, resetDatabase } from './test-helpers.js';
 
 describe('getUserByUsername', () => {
+    afterEach('reset the database', async () => {
+        await resetDatabase();
+    });
+
     it('get the correct user from the database given a username', async () => {
         const fakeData = [{
             id: '123',
@@ -18,7 +22,6 @@ describe('getUserByUsername', () => {
 
         const actual = await getUserByUsername('abc');
         const finalDBState = await getDatabaseData('users');
-        await resetDatabase();
 
         const expected = {
             id: '123',
@@ -28,5 +31,9 @@ describe('getUserByUsername', () => {
 
         expect(actual).excludingEvery('_id').to.deep.equal(expected);
         expect(finalDBState).excludingEvery('_id').to.deep.equal(fakeData);
+    });
+
+    it('returns null when the user is not found', async () => {
+
     });
 });
